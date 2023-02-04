319b8703293f4bc9c394693a3958b8e85470126d
Choux <79139661+sykality@users.noreply.github.com>
2023-02-04 16:10:20 +0900

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -4,34 +4,39 @@ const ideologies = { // eslint-disable-line no-unused-vars
     b: -1,
     c: -1,
     d: -1,
-    e: -1
+    e: -1,
+    f: -1
   },
   'liberal-feminist': {
     a: -1,
     b: 101,
     c: -1,
     d: -1,
-    e: -1
+    e: -1,
+    f: -1
   },
   'radical-feminist': {
     a: -1,
     b: -1,
     c: 101,
     d: -1,
-    e: -1
+    e: -1,
+    f: -1
   },
   'socialist-feminist': {
     a: -1,
     b: -1,
     c: -1,
     d: 101,
-    e: -1
+    e: -1,
+    f: -1
   },
   'queer-feminist': {
     a: -1,
     b: -1,
     c: -1,
     d: -1,
-    e: 101
+    e: 101,
+    f: -1
   }
 }
