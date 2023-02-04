3d155300710c983a583f68ec49f4c7f657889485
Choux <79139661+sykality@users.noreply.github.com>
2023-02-04 15:54:25 +0900

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,211 @@
+const questions = { // eslint-disable-line no-unused-vars
+  question_0: {
+    a: 1,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0
+    f: 0
+   },
+ question_1: {
+    a: 1,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0
+    f: 0
+   },
+ question_2: {
+    a: 1,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0
+    f: 0
+   },
+ question_3: {
+    a: 1,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_4: {
+    a: 1,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_5: {
+    a: 0,
+    b: 1,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_6: {
+    a: 0,
+    b: 1,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_7: {
+    a: 0,
+    b: 1,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_8: {
+    a: 0,
+    b: 1,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_9: {
+    a: 0,
+    b: 1,
+    c: 0,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_10: {
+    a: 0,
+    b: 0,
+    c: 1,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_11: {
+    a: 0,
+    b: 0,
+    c: 1,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_12: {
+    a: 0,
+    b: 0,
+    c: 1,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_13: {
+    a: 0,
+    b: 0,
+    c: 1,
+    d: 0,
+    e: 0,
+    f: 0
+   },
+ question_14: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 1,
+    e: 0,
+    f: 0
+   },
+ question_15: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 1,
+    e: 0,
+    f: 0
+   },
+ question_16: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 1,
+    e: 0,
+    f: 0
+   },
+ question_17: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 1,
+    e: 0,
+    f: 0
+   },
+ question_18: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 1,
+    e: 0,
+    f: 0
+   },
+ question_19: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 1,
+    e: 0,
+    f: 0
+   },
+ question_20: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 1,
+    f: 0
+   },
+ question_21: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 1,
+    f: 0
+   },
+ question_22: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 1,
+    f: 0
+   },
+ question_23: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 1,
+    f: 0
+   },
+ question_24: {
+    a: 0,
+    b: 0,
+    c: 0,
+    d: 0,
+    e: 1,
+    f: 0
+   }
+}
+
+Object.size = function (obj) {
+  let size = 0
+  let key
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) size++
+  }
+  return size
+}
