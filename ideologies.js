e09bf1f71a9f8377a5c3a81e3d8b2ac0105fcb8d
Choux <79139661+sykality@users.noreply.github.com>
2023-02-04 14:40:01 +0900

    Create ideologies.js

---
+++ ideologies.js
@@ -0,0 +1,37 @@
+const ideologies = { // eslint-disable-line no-unused-vars
+  'conservative': {
+    a: 101,
+    b: -1,
+    c: -1,
+    d: -1,
+    e: -1
+  },
+  'liberal-feminist': {
+    a: -1,
+    b: 101,
+    c: -1,
+    d: -1,
+    e: -1
+  },
+  'radical-feminist': {
+    a: -1,
+    b: -1,
+    c: 101,
+    d: -1,
+    e: -1
+  },
+  'socialist-feminist': {
+    a: -1,
+    b: -1,
+    c: -1,
+    d: 101,
+    e: -1
+  },
+  'queer-feminist': {
+    a: -1,
+    b: -1,
+    c: -1,
+    d: -1,
+    e: 101
+  }
+}
